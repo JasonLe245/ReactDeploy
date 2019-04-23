84087e55222773e41e08ac865fb6f54a9e1660d9
Le Dinh Minh Long <jason@student-10-201-37-195.wireless.adelaide.edu.au>
2019-04-23 13:57:12 +0930

    Updates

---
+++ precache-manifest.ebd4bf2eecaba1986751068c620c03c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "138d9f1f539b7b7244a301f20f9d6883",
+    "url": "/JasonLe245/ReactDeploy/index.html"
+  },
+  {
+    "revision": "8f63170a60a00e01bb35",
+    "url": "/JasonLe245/ReactDeploy/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/JasonLe245/ReactDeploy/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "8f63170a60a00e01bb35",
+    "url": "/JasonLe245/ReactDeploy/static/js/main.4e7025d0.chunk.js"
+  },
+  {
+    "revision": "4518a1f66ccaec58d76e",
+    "url": "/JasonLe245/ReactDeploy/static/js/runtime~main.c716f868.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/JasonLe245/ReactDeploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
